d9de3f830c1049cdbaa4eacfdf3f1e980b42fabe
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2023-09-26 16:58:35 -0400

    Restaurant-domain-core: Move publish method to DomainEvent

--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/restaurant/domain/event/OrderApprovalEvent.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/restaurant/domain/event/OrderApprovalEvent.java
@@ -17,6 +17,4 @@ public abstract class OrderApprovalEvent implements DomainEvent<OrderApproval> {
     private final RestaurantId restaurantId;
     private final List<String> failureMessages;
     private final ZonedDateTime createdAt;
-
-    public abstract void publish();
 }
