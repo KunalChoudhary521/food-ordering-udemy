770ce14d24ea38011e6f0f2cbd22197900c5581f
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2023-10-03 18:31:15 -0400

    Kafka-producer: Add outbox callback on success or failure to publish

--- infrastructure/kafka/producer/src/main/java/com/food/ordering/kafka/producer/KafkaMessagePublisher.java
+++ infrastructure/kafka/producer/src/main/java/com/food/ordering/kafka/producer/KafkaMessagePublisher.java
@@ -1,5 +1,10 @@
 package com.food.ordering.kafka.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.food.ordering.order.domain.exception.OrderDomainException;
+import com.food.ordering.outbox.OutboxStatus;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
@@ -7,18 +12,35 @@ import org.springframework.stereotype.Component;
 import java.util.function.BiConsumer;
 
 @Component
+@AllArgsConstructor
 @Slf4j
 public class KafkaMessagePublisher {
 
-    public <T> BiConsumer<SendResult<String, T>, Throwable> getKafkaCallback(String topic, T avroModel,
-                                                                             String orderId, String avroModelName) {
+    private final ObjectMapper objectMapper;
+
+    public <T> T getOrderEventPayload(String payload, Class<T> outputType) {
+        try {
+            return objectMapper.readValue(payload, outputType);
+        } catch (JsonProcessingException e) {
+            String errorMessage = String.format("Could not read %s object!", outputType.getName());
+            log.error(errorMessage, e);
+            throw new OrderDomainException(errorMessage, e);
+        }
+    }
+
+    public <T, U> BiConsumer<SendResult<String, T>, Throwable> getKafkaCallback(String topic, T avroModel, U outboxMessage,
+                                                                                BiConsumer<U, OutboxStatus> outboxCallback,
+                                                                                String orderId, String avroModelName) {
         return (result, ex) -> {
             if (ex == null) {
                 log.info("Successfully received response from Kafka for order id {} Topic: {} Partition: {} Offset {} Timestamp {}",
                         orderId, topic, result.getRecordMetadata().partition(), result.getRecordMetadata().offset(),
                         result.getRecordMetadata().timestamp());
+                outboxCallback.accept(outboxMessage, OutboxStatus.COMPLETED);
             } else {
-                log.error("Failed to publish {} message {} to topic {}", avroModelName, avroModel.toString(), topic, ex);
+                log.error("Error while publishing {} with message: {} and outbox type: {} to topic {}",
+                        avroModelName, avroModel.toString(), outboxMessage.getClass().getName(), topic, ex);
+                outboxCallback.accept(outboxMessage, OutboxStatus.FAILED);
             }
         };
 
