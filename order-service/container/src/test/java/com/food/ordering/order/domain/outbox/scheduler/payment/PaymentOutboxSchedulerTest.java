a9a4e3ea740eca9ae024c234698ab97fb6856353
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2023-12-12 15:19:16 -0500

    Order-container: Mock KafkaTemplate for payment outbox IT

--- order-service/container/src/test/java/com/food/ordering/order/domain/outbox/scheduler/payment/PaymentOutboxSchedulerTest.java
+++ order-service/container/src/test/java/com/food/ordering/order/domain/outbox/scheduler/payment/PaymentOutboxSchedulerTest.java
@@ -5,30 +5,30 @@ import com.food.ordering.order.container.OrderServiceApplication;
 import com.food.ordering.order.data.access.outbox.payment.entity.PaymentOutboxEntity;
 import com.food.ordering.order.data.access.outbox.payment.repository.PaymentOutboxJpaRepository;
 import com.food.ordering.outbox.OutboxStatus;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
-import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.time.Duration;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = OrderServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@EmbeddedKafka(topics = "${order-service.payment-request-topic}", partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
 class PaymentOutboxSchedulerTest {
 
@@ -37,14 +37,8 @@ class PaymentOutboxSchedulerTest {
     @Value("${order-service.payment-request-topic}")
     private String paymentRequestTopic;
 
-    @Value("${kafka-consumer-config.payment-consumer-group-id}")
-    private String paymentConsumerGroupId;
-
-    @Autowired
-    private EmbeddedKafkaBroker embeddedKafkaBroker;
-
-    @Autowired
-    private ConsumerFactory<String, PaymentRequest> consumerFactory;  // kafka properties used from KafkaConsumerConfig
+    @MockBean
+    private KafkaTemplate<String, PaymentRequest> kafkaTemplate;
 
     @Autowired
     private PaymentOutboxJpaRepository paymentOutboxJpaRepository;
@@ -52,27 +46,22 @@ class PaymentOutboxSchedulerTest {
     @Autowired
     private PaymentOutboxScheduler paymentOutboxScheduler;
 
-    private Consumer<String, PaymentRequest> consumer;
-
-    @BeforeAll
-    public void setUp() {
-        consumer = consumerFactory.createConsumer(paymentConsumerGroupId, "");
-        embeddedKafkaBroker.consumeFromAnEmbeddedTopic(consumer, paymentRequestTopic);
-    }
-
-    @AfterAll
-    public void tearDown() {
-        consumer.close();
-    }
-
     @Test
     void orderPaymentOutboxMessage_processOutboxMessage_publishMessageAndUpdateOutboxStatus() {
+        CompletableFuture<SendResult<String, PaymentRequest>> futureResult = CompletableFuture.completedFuture(createSendResult());
+
+        when(kafkaTemplate.send(eq(paymentRequestTopic), anyString(), any(PaymentRequest.class))).thenReturn(futureResult);
+
         paymentOutboxScheduler.processOutboxMessage();
-        KafkaTestUtils.getRecords(consumer, Duration.ofSeconds(2L), 1);
 
         Optional<PaymentOutboxEntity> processedOutboxMessage = paymentOutboxJpaRepository.findById(PAYMENT_OUTBOX_ID);
-
         assertTrue(processedOutboxMessage.isPresent());
         assertEquals(OutboxStatus.COMPLETED, processedOutboxMessage.get().getOutboxStatus());
+    }
+
+    private SendResult<String, PaymentRequest> createSendResult() {
+        ProducerRecord<String, PaymentRequest> producerRecord = new ProducerRecord<>("", "", new PaymentRequest());
+        RecordMetadata recordMetadata = new RecordMetadata(new TopicPartition("", 1), 1L, 1, 1L, 1, 1);
+        return new SendResult<>(producerRecord, recordMetadata);
     }
 }
